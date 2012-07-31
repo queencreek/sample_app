e41aa4a23475a0d0fca3d7ee47ac1c6716029adc
Queen Creek <git@queencreek.com>
2012-07-31 01:35:30 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '96049890a32f86aa2fae88959dee9792316930d4f2cb50cc4b5bdbe4285b6cb4cfdc19203714a45d7e5281f3149461ad075799716260f4b3f60f6419b59c4cd9'
